7b31924fb9ad8ace9e0a25953f94fbeed5dfc08f
Florian Thiel <flth@flth.de>
2023-05-04 14:59:40 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,28 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "DR 137 / DB VT 90",
+		["mod_desc"] = "After the two electric \"glass trains\" of the ET 91 had been in successful service since 1935, however, its range of use was limited to lines with contact wire. The roof could not be set up to open because of the current collectors. This was the reason for the DRG to develop similar vehicles with diesel-hydraulic power transmission. The railcars were to be usable independently of the overhead wire and be able to run on scenic main lines and branch lines. As a special feature, a roof with an opening roll top was to provide an equally unobstructed view as on the observation bus. The Deutsche Bundesbahn took over two vehicles, and they were taken out of service by 1962. None of the vehicles was preserved for posterity.",
+        ["vt90_menu"] = "DR 137 / DB VT 90",
+
+		["vt90_desc"] = "The observation railcar 137 240 and the three years later almost identically constructed 137 462 and 463 were diesel railcars of the Deutsche Reichsbahn for excursion traffic.",
+		["vt90_name1"] = "DR VT 137 240 (1936)",
+		["vt90_name2"] = "DB VT 90 (1947)",
+		["vt90_name3"] = "DR VT 137 462 (1939)",
+
+	},
+	de = {
+
+		["mod_name"] = "DR 137 / DB VT 90",
+		["mod_desc"] = "Nachdem die beiden elektrischen \"Gläsernen Züge\" des ET 91 seit 1935 erfolgreich im Einsatz waren, allerdings war sein Einsatzbereich auf Strecken mit Fahrdraht beschränkt. Das Dach konnte wegen der Stromabnehmer nicht zum Öffnen eingerichtet werden. Das war für die DRG der Anlass zur Entwicklung ähnlicher Fahrzeuge mit dieselhydraulischer Kraftübertragung. Die Triebwagen sollten unabhängig vom Fahrdraht einsetzbar sein und auf landschaftlich reizvollen Haupt- und Nebenbahnen fahren können. Als Besonderheit sollte ein Dach mit öffnungsfähigem Rollverdeck eine ebenso freie Aussicht wie beim Aussichtsomnibus gewähren. Die Deutsche Bundesbahn übernahm zwei Fahrzeuge, bis 1962 wurden sie ausgemustert. Keines der Fahrzeuge blieb der Nachwelt erhalten.",
+        ["vt90_menu"] = "DR 137 / DB VT 90",
+
+		["vt90_desc"] = "Der Aussichtstriebwagen 137 240 und die drei Jahre später nahezu baugleich ausgeführten 137 462 und 463 waren Dieseltriebwagen der Deutschen Reichsbahn für den Ausflugsverkehr.",
+		["vt90_name1"] = "DR VT 137 240 (1936)",
+		["vt90_name2"] = "DB VT 90 (1947)",
+		["vt90_name3"] = "DR VT 137 462 (1939)",
+
+	},
+}
+end
