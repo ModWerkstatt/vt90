dc803f8e90da3a89dacaa8a8b397ff342715dbef
Florian <flth@flth.de>
2026-06-24 00:20:43 +0200

    Revert "Konvertierung"
    This reverts commit 3f2b169e54efb488f7bc741ff9ed8aec72e6fe72.

---
+++ mod.lua
@@ -0,0 +1,28 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "electric", "diesel", "deutschland", "germany", "triebwagen", "bundesbahn", "drg" },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
